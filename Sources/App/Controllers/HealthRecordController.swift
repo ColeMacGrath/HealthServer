070faf8a3076d50fd057dd0ae51306b6c2445dcc
Moisés Córdova <kernel_linux@outlook.com>
2022-01-23 23:17:20 -0600

    Update HealthRecordController.swift

--- Sources/App/Controllers/HealthRecordController.swift
+++ Sources/App/Controllers/HealthRecordController.swift
@@ -11,8 +11,10 @@ import Fluent
 struct HealthRecordController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let healthRecordRoute = routes.grouped("records")
+        healthRecordRoute.get("getAll", use: getAllRecords)
         healthRecordRoute.post("create", use: create)
-        healthRecordRoute.post("getAll", use: getAllRecordsFor)
+        healthRecordRoute.post("createMultiple", use: createMultipleRecords)
+        healthRecordRoute.post("getFor", use: getAllRecordsFor)
     }
     
     func create(req: Request) throws -> EventLoopFuture<HTTPStatus> {
@@ -24,8 +26,23 @@ struct HealthRecordController: RouteCollection {
         }
     }
     
-    func getAllRecordsFor(_ req: Request) throws -> EventLoopFuture<[HealthRecord]> {
-        let userId = try req.content.decode(UserId.self)
+    func createMultipleRecords(req: Request) throws -> EventLoopFuture<HTTPStatus> {
+        let healthRecordData = try req.content.decode([HealthRecordData].self).map({ record -> HealthRecord in
+            return try HealthRecord.create(from: record)
+        })
+        return healthRecordData.create(on: req.db).transform(to: HTTPStatus.created)
+    }
+    
+    func getAllRecords(_ req: Request) throws -> EventLoopFuture<[HealthRecord]> {
         return HealthRecord.query(on: req.db).all()
+    }
+    
+    func getAllRecordsFor(_ req: Request) throws -> EventLoopFuture<[HealthRecord]> {
+        let userId = try req.content.decode(UserId.self).user_id
+        let healthRecords = HealthRecord.query(on: req.db).all().map { records in
+            records.filter({ $0.$user.id == userId })
+        }
+        
+        return healthRecords
     }
 }
